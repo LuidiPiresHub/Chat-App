63f6ccc220fa8ea3f5b181f06c6b7537d0339ad5
LuidiPires <luidihot@gmail.com>
2025-05-22 06:29:52 -0300

    refactor: mensagem de erro em português

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -2,5 +2,5 @@ import { Request, Response, NextFunction } from 'express';
 
 export const errorHandler = (err: Error, _req: Request, res: Response, _next: NextFunction) => {
   console.error(err);
-  res.status(500).json({ message: 'Internal Server Error' });
+  res.status(500).json({ message: 'Erro interno no sistema' });
 };
