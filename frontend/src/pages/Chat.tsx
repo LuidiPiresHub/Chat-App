76f4d1c904b40aeca1be9ce7ddb3906c935c5c1a
LuidiPires <luidihot@gmail.com>
2025-05-24 20:52:48 -0300

    feat: balão de mensagens

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -100,7 +100,7 @@ export default function Chat() {
 
   return (
     <main className='h-dvh flex'>
-      <aside ref={asideRef} className={`bg-[#141428] h-dvh absolute ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'} transition md:static md:translate-x-0 w-full max-w-70 flex flex-col gap-4 border-r border-gray-800 p-4 select-none`}>
+      <aside ref={asideRef} className={`bg-[#141428] h-dvh absolute z-1 ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'} transition md:static md:translate-x-0 w-full max-w-70 flex flex-col gap-4 border-r border-gray-800 p-4 select-none`}>
         <h1 className='text-4xl font-bold'>Chat</h1>
         <button className='bg-gray-700 rounded-lg py-3 px-3 gap-3 flex items-center cursor-pointer'>
           <Plus className='size-5' />
@@ -112,7 +112,7 @@ export default function Chat() {
             type="text"
             placeholder='Search a friend...'
             className='bg-gray-700 w-full p-3 rounded-lg outline-none'
-            onChange={({ target: { value }}) => setSearch(value.trim())}
+            onChange={({ target: { value } }) => setSearch(value.trim())}
           />
           <Search className='absolute size-5 right-4 top-1/2 transform -translate-y-1/2' />
         </label>
@@ -142,25 +142,34 @@ export default function Chat() {
           <UserCircle2 className='size-12' />
           <h2 className='text-2xl font-bold'>Alice</h2>
         </header>
-        <section className='flex flex-1 flex-col gap-4 p-4 overflow-y-scroll scrollbar'>
-          {messages.map((message) => (
-            <div key={message.id} className={`flex items-center gap-2 ${message.sender === 'Alice' ? 'justify-end' : 'justify-start'}`}>
-              <p className={`rounded-lg p-3 max-w-[80%] sm:max-w-[60%] ${message.sender === 'Alice' ? 'bg-blue-700' : 'bg-gray-700'}`}>
-                {message.text}
-              </p>
-            </div>
-          ))}
+        <section className="flex flex-1 flex-col gap-4 p-4 overflow-y-scroll scrollbar">
+          {messages.map((message, index) => {
+            const isCurrentUser = message.sender === 'Alice';
+            const messageAlignment = isCurrentUser ? 'justify-end' : 'justify-start';
+            const messageStyles = isCurrentUser ? 'bg-blue-700 rounded-tr-none' : 'bg-gray-700 rounded-tl-none';
+            const tailStyles = isCurrentUser ? 'border-t-blue-700 -right-2' : 'border-t-gray-700 -left-2';
+
+            return (
+              <div key={index} className={`flex ${messageAlignment} px-4 py-2`}>
+                <div className={`relative ${messageStyles} text-white rounded-lg p-3 max-w-[60%]`}>
+                  <div className={`absolute top-0 w-0 h-0 ${tailStyles} border-t-8 border-l-8 border-r-8 border-l-transparent border-r-transparent`}></div>
+                  <p className="wrap-anywhere">{message.text}</p>
+                  <span className="text-xs text-white/80 block text-right mt-1">11:39</span>
+                </div>
+              </div>
+            );
+          })}
           <div ref={messagesEndRef} />
         </section>
         <form className='flex items-center gap-2 p-4 border-t border-gray-800'>
           <input
             type="text"
             placeholder='Type a message...'
-            className='bg-gray-700 rounded-lg py-3 px-5 flex-1 outline-none'
+            className='bg-gray-700 rounded-lg py-3 px-5 flex-1 outline-none w-full min-w-[170px]'
           />
           <button
             type="submit"
-            className='bg-blue-700 rounded-lg py-3 px-5 flex items-center justify-center cursor-pointer'
+            className='bg-blue-700 rounded-lg py-3 px-5 cursor-pointer'
           >
             <Send />
           </button>
