575a3ad59d3916a834f8789973d5daf2ff008bbd
LuidiPires <luidihot@gmail.com>
2025-05-26 16:04:47 -0300

    feat: nova validação para username

--- frontend/src/schemas/signUp.ts
+++ frontend/src/schemas/signUp.ts
@@ -2,7 +2,11 @@ import { z } from 'zod';
 import { signInSchema } from './signIn';
 
 export const signUpSchema = signInSchema.extend({
-  username: z.string().min(3, 'Usuário deve ter pelo menos 3 caracteres').regex(/^[^\d].*$/, 'Usuário não pode começar com número'),
+  username: z
+    .string()
+    .min(3, 'Usuário deve ter pelo menos 3 caracteres')
+    .regex(/^[^\d].*$/, 'Usuário não pode começar com número')
+    .regex(/^[a-zA-Z0-9]+(_[a-zA-Z0-9]+)*$/, 'Use "_" entre palavras, sem espaços ou caracteres especiais'),
   confirmPassword: z.string().min(6, 'Senha deve ter pelo menos 6 caracteres'),
 }).refine(({ password, confirmPassword }) => password === confirmPassword, {
   message: 'As senhas não coincidem',
