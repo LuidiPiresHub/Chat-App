308d2abb51a2b207c8361c6de76f941d9fe619ce
LuidiPires <luidihot@gmail.com>
2025-06-05 05:08:23 -0300

    fix: schema não permite numeros no inicio ou espaços vazios

--- backend/src/schemas/user.ts
+++ backend/src/schemas/user.ts
@@ -1,5 +1,9 @@
 import { z } from 'zod';
 
 export const userDisplayNameSchema = z.object({
-  displayName: z.string().min(3, 'O nome de exibição precisa ter pelo menos 3 caracteres').max(20, 'O nome de exibição precisa ser menor que 20 caracteres')
+  displayName: z
+    .string()
+    .min(3, 'O nick deve ter pelo menos 3 caracteres')
+    .regex(/^[^\d].*$/, 'Usuário não pode começar com número')
+    .regex(/^[a-zA-Z0-9]+(_[a-zA-Z0-9]+)*$/, 'Use "_" entre palavras, sem espaços ou caracteres especiais'),
 });
