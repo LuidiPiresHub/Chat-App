73aa8c7abb28e532ac7adc22f1ab524ddd7eaa4f
LuidiPires <luidihot@gmail.com>
2025-06-05 04:25:54 -0300

    feat: toastfy

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,15 +4,19 @@ import { BrowserRouter } from 'react-router-dom';
 import AuthProvider from './contexts/Auth/AuthProvider.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './index.css';
+import { ToastContainer } from 'react-toastify';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-    <AuthProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AuthProvider>
-  </QueryClientProvider>
+  <>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
+    </QueryClientProvider>
+    <ToastContainer />
+  </>
 );
