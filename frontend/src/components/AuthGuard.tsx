68528b2d52fc92e7ad00b7e4dc7681b271e7af32
LuidiPires <luidihot@gmail.com>
2025-09-14 19:43:36 -0300

    refactor: remove ultima url de retorno, mantem rota "/" como principal

--- frontend/src/components/AuthGuard.tsx
+++ frontend/src/components/AuthGuard.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
 import LoadingScreen from '../pages/LoadingScreen';
 import { useEffect, useState } from 'react';
@@ -9,10 +9,7 @@ interface IAuthGuardProps {
 
 export default function AuthGuard({ isProtected = false }: IAuthGuardProps) {
   const { isAuthenticated, isLoading } = useAuth();
-  const { state, pathname } = useLocation();
   const [showLoading, setShowLoading] = useState<boolean>(false);
-
-  const from = state?.from || '/';
 
   useEffect(() => {
     const timer = setTimeout(() => setShowLoading(true), 200);
@@ -24,8 +21,8 @@ export default function AuthGuard({ isProtected = false }: IAuthGuardProps) {
   }
 
   if (isProtected) {
-    return isAuthenticated ? <Outlet /> : <Navigate to="/sign-in" state={{ from: pathname }} replace={true} />;
+    return isAuthenticated ? <Outlet /> : <Navigate to="/sign-in" replace={true} />;
   }
 
-  return isAuthenticated ? <Navigate to={from} replace={true} /> : <Outlet />;
+  return isAuthenticated ? <Navigate to="/" replace={true} /> : <Outlet />;
 }
