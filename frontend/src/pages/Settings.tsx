e6bab9bc1e9ea91168dc94ae6f27bedbcaf613d1
LuidiPires <luidihot@gmail.com>
2025-09-14 18:45:31 -0300

    fix: tipo corrigido da função formatDate

--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -47,7 +47,7 @@ export default function Settings() {
     }
   });
 
-  const formatDate = (dateString: string): string => {
+  const formatDate = (dateString: Date): string => {
     const date = new Date(dateString);
     const day = String(date.getDate()).padStart(2, '0');
     const month = String(date.getMonth() + 1).padStart(2, '0');
